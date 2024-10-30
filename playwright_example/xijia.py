59550884da594a695f1ad265d8fa394a150af84f
White1ion <whitelion8g1@gmail.com>
2024-10-30 18:15:00 +0800

    Update xijia.py
    请求删除AmorePacific相关域名，谢谢

--- playwright_example/xijia.py
+++ playwright_example/xijia.py
@@ -10,29 +10,29 @@ def run(playwright):
     # Open new page
     page = context.newPage()
 
-    # Go to http://rms.amorepacific.com.cn/Aspx/Web/LoginNew.aspx
-    page.goto("http://rms.amorepacific.com.cn/Aspx/Web/LoginNew.aspx")
+    # Go to http://
+    page.goto("http://")
 
     # Click input[name="txtUsername"]
     page.click("input[name=\"txtUsername\"]")
 
     # Fill input[name="txtUsername"]
-    page.fill("input[name=\"txtUsername\"]", "CN35008943")
+    page.fill("input[name=\"txtUsername\"]", "account")
 
     # Click input[name="txtPass"]
     page.click("input[name=\"txtPass\"]")
 
     # Fill input[name="txtPass"]
-    page.fill("input[name=\"txtPass\"]", "35008943")
+    page.fill("input[name=\"txtPass\"]", "passwd")
 
     # Click input[name="btnLogin"]
     page.click("input[name=\"btnLogin\"]")
-    # assert page.url == "http://rms.amorepacific.com.cn/admin/default.asp"
+    # assert page.url == "http://"
 
 
     # Open new page
     page1 = context.newPage()
-    page1.goto('http://rms.amorepacific.com.cn/admin/left.asp?menu=12')
+    page1.goto('http://')
 
     print(page.frames)
 
@@ -44,7 +44,7 @@ def run(playwright):
 
     # Click text="CIQ Details"
     page2.click("text=\"CIQ Details\"")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq_detail/Search.aspx"
+    # assert page2.url == "http://"
 
 
     # Upload 表格.xlsx
@@ -55,14 +55,14 @@ def run(playwright):
     page2.on("dialog", lambda dialog: (print("Dialog %s" % dialog.message),
                                       dialog.accept(),
                                       ))
-    # with page2.expect_navigation(url="http://rms.amorepacific.com.cn/Aspx/Web/t_ciq_detail/Search.aspx"):
+    # with page2.expect_navigation(url="http://"):
     with page2.expect_navigation():
         # 点击 Upload
         page2.click("input[name=\"ctl00$ContentPlaceHolder1$Button1\"]")
 
     # Click text="CIQ Batch Files"
     page2.click("text=\"CIQ Batch Files\"")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/Search.aspx"
+    # assert page2.url == "http://"
 
     # Click input[name="ctl00$head$CiqContent$txtciq_id"]
     page2.click("input[name=\"ctl00$head$CiqContent$txtciq_id\"]")
@@ -70,7 +70,7 @@ def run(playwright):
     # Fill input[name="ctl00$head$CiqContent$txtciq_id"]
     page2.fill("input[name=\"ctl00$head$CiqContent$txtciq_id\"]", "RY570")
 
-    # Go to http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/Search.aspx
+    # Go to http://
     page2.click('//*[@id="ctl00_head_CiqContent_btnSearch"]')
 
 
@@ -78,60 +78,60 @@ def run(playwright):
     # ---------------------------------
     # Click text="Upload File"
     page2.click('//*[@id="ctl00_ContentPlaceHolder1_gridView"]/tbody/tr[2]/td[7]/a')
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/ModifyFiles.aspx?id=10202&mark=ciq"
+    # assert page2.url == "http://"
 
     # Upload 卫生证书.pdf
     page2.setInputFiles("input[name=\"ctl00$ContentPlaceHolder1$FileUpload1\"]", r"C:\Users\EDZ\Desktop\解压缩文件夹\RY570\CIQ证书\卫生证书.pdf")
 
     # Click input[name="ctl00$ContentPlaceHolder1$btnUpdate"]
     page2.click("input[name=\"ctl00$ContentPlaceHolder1$btnUpdate\"]")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/ModifyFiles.aspx?id=10202&mark=ciq"
+    # assert page2.url == "http://"
 
     # Click input[name="ctl00$ContentPlaceHolder1$btnReturn"]
     page2.click("input[name=\"ctl00$ContentPlaceHolder1$btnReturn\"]")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/Search.aspx"
+    # assert page2.url == "http://"
     # ---------------------------------
 
 
     # ---------------------------------
     # Click //td[8]/a[normalize-space(.)='Upload File']
     page2.click('//*[@id="ctl00_ContentPlaceHolder1_gridView"]/tbody/tr[2]/td[8]/a')
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/ModifyFiles.aspx?id=10202&mark=customs"
+    # assert page2.url == "http://"
 
     # Upload 222520201000305059报关单.pdf
     page2.setInputFiles("input[name=\"ctl00$ContentPlaceHolder1$FileUpload1\"]", r"C:\Users\EDZ\Desktop\解压缩文件夹\RY570\报关单\222520201000305059报关单.pdf")
 
     # Click input[name="ctl00$ContentPlaceHolder1$btnUpdate"]
     page2.click("input[name=\"ctl00$ContentPlaceHolder1$btnUpdate\"]")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/ModifyFiles.aspx?id=10202&mark=customs"
+    # assert page2.url == "http://"
 
     # Click input[name="ctl00$ContentPlaceHolder1$btnReturn"]
     page2.click("input[name=\"ctl00$ContentPlaceHolder1$btnReturn\"]")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/Search.aspx"
+    # assert page2.url == "http://"
     # ---------------------------------
 
 
     # ---------------------------------
     # Click //td[9]/a[normalize-space(.)='Upload File']
     page2.click('//*[@id="ctl00_ContentPlaceHolder1_gridView"]/tbody/tr[2]/td[9]/a')
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/ModifyFiles.aspx?id=10202&mark=textbill"
+    # assert page2.url == "http://"
 
     # Upload 税单.pdf
     page2.setInputFiles("input[name=\"ctl00$ContentPlaceHolder1$FileUpload1\"]", r"C:\Users\EDZ\Desktop\解压缩文件夹\RY570\税单.pdf")
 
     # Click input[name="ctl00$ContentPlaceHolder1$btnUpdate"]
     page2.click("input[name=\"ctl00$ContentPlaceHolder1$btnUpdate\"]")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/ModifyFiles.aspx?id=10202&mark=textbill"
+    # assert page2.url == "http://"
 
     # Click input[name="ctl00$ContentPlaceHolder1$btnReturn"]
     page2.click("input[name=\"ctl00$ContentPlaceHolder1$btnReturn\"]")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/Search.aspx"
+    # assert page2.url == "http://"
     # ---------------------------------
 
 
     # Click text="CIQ Details"
     page2.click("text=\"CIQ Details\"")
-    # assert page2.url == "http://rms.amorepacific.com.cn/Aspx/Web/t_ciq_detail/Search.aspx"
+    # assert page2.url == "http://"
 
     # Click input[name="ctl00$head$CiqContent$txtciq_id"]
     page2.click("input[name=\"ctl00$head$CiqContent$txtciq_id\"]")
@@ -139,7 +139,7 @@ def run(playwright):
     # Fill input[name="ctl00$head$CiqContent$txtciq_id"]
     page2.fill("input[name=\"ctl00$head$CiqContent$txtciq_id\"]", "RY570")
 
-    # Go to http://rms.amorepacific.com.cn/Aspx/Web/t_ciq/Search.aspx
+    # Go to http://
     page2.click('//*[@id="ctl00_head_CiqContent_btnSearch"]')
 
 
